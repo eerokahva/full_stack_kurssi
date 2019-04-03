518e8194b8d0afa89962ee56a96c4ca48e1cc9a1
eerokahva <eero.kahva@gmail.com>
2019-04-03 22:35:56 +0300

    1.5 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header =(props) => {
+const Header = (props) => {
     return(
         <div>
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         </div>
     )
 }
+
 
 const Part = (props) => {
     return(
@@ -20,37 +21,46 @@ const Part = (props) => {
 const Content = (props) => {
     return(
         <div>
-            <Part part={props.part1} exercises={props.exercises1}/>
-            <Part part={props.part2} exercises={props.exercises2}/>
-            <Part part={props.part3} exercises={props.exercises3}/>
+            <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+            <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+            <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
         </div>
     )
 }
 
-//TESTI
-
 const Total =(props) => {
+    console.log(props)
     return(
         <div>
-            <p>Yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
+            <p>Yhteensä {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} tehtävää</p>
         </div>
     )
 }
 
 const App =() => {
-    const course = "Half Stack -sovelluskehitys"
-    const part1 = "Reactin perusteet"
-    const exercises1 = 10
-    const part2 = "Tiedonvälitys propseilla"
-    const exercises2 = 7
-    const part3 = "Komponenttien tila"
-    const exercises3 = 14
+    const course = {
+        name: "Half Stack -sovelluskehitys",
+        parts: [
+            {
+                name: "Reactin perusteet",
+                exercises: 10
+            },
+            {
+                name: "Tiedonvälitys propseilla",
+                exercises: 7
+            },
+            {
+                name: "Komponenttien tila",
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />            
+            <Content course={course} />
+            <Total course={course} />
         </div>
     )
 }
